1dd84f1c73c9deba95154450480aa714f35a4b30
Albert Lin <Albertkuilin@gmail.com>
2018-07-19 13:20:59 -0700

    implemented a second model, with multiple inputs concatenated together, allows you to run either model, memory expanded to 20,000 from 2,000, e-decay now 0.9988 instead of 0.997, batch size decreased to 32, allowing saving board history for model playing, not only training, will save final model once training is done now

--- ReinforcementDeepLearningforPlayingSnake.py
+++ ReinforcementDeepLearningforPlayingSnake.py
@@ -11,6 +11,14 @@ and trained an AI to play it through reinforcement deep learning
 import numpy as np
 import random
 
+print("Disable print statements? (y/n) - IMPLEMENT this for more effeciency!")
+isPrinting = input()
+print("Which model? (1 or 2, default is 2 (more input info))")
+modeltype = int(input())
+if modeltype == 1:
+    print("Using model 1")
+else:
+    print("Using model 2")
 print("Which mode to run this in? (T)rain/(U)serplay/(M)odelplay/(W)atch")
 mode = input()
 
@@ -47,7 +55,10 @@ class Snake():
         self.boardSize=boardSize
         self.maxDist = (boardSize-1)*2
         self.startingSize = startingSize
-        self.numStateInputs = ((boardSize, boardSize, 3), )
+        if modeltype==1:
+            self.numStateInputs = ((boardSize, boardSize, 3), )
+        else:
+            self.numStateInputs = ((boardSize, boardSize, 3), (4, ), (2, ))
         self.numActions = 4
         self.minSnakeColor = 0.5
         self.reset()
@@ -79,9 +90,18 @@ class Snake():
         IMPORTANT NOTE:
             need to supply inputs as (samples, dim, dim, ..., features)
             as ONE array, not a tuple of arrays
+            
+        also return displacement from board edges and apple if modeltype is not 1
+        multiple inputs should be put into one array (NOT TUPLE)
         '''
-        return np.reshape(self.board, (1, ).__add__(self.numStateInputs[0]))
-    
+        
+        if modeltype == 1:
+            return np.reshape(self.board, (1, ).__add__(self.numStateInputs[0]))
+        else:
+            dispFromEdges = np.array([[self.positionList[-1, 0]+1, self.boardSize-self.positionList[-1, 1], self.boardSize-self.positionList[-1, 0], self.positionList[-1, 1]+1]], dtype='float32')
+            dispFromApple = np.array([[self.applePos[0]-self.positionList[-1, 0], self.applePos[1]-self.positionList[-1, 1]]], dtype='float32')
+            return [np.reshape(self.board, (1, ).__add__(self.numStateInputs[0])), dispFromEdges, dispFromApple]
+        
     def takeAction(self, action):
         '''
         
@@ -243,7 +263,14 @@ if mode.__eq__("U"):
         tick, score = env.getGameInfo()
         print("Current Tick: {}".format(tick))
         print("Current Score: {}".format(score))
-        imgplot = plt.imshow(state[0])
+        reward = env.getCurrentReward()
+        print("Current Reward: {}".format(reward))
+        
+        if modeltype != 1:
+            imgplot = plt.imshow(state[0][0])
+            print("State inputs other than board: {}, {}".format(state[1][0], state[2][0]))
+        else:
+            imgplot = plt.imshow(state[0])
         plt.pause(0.001)
         alive = True
         while alive:
@@ -260,7 +287,11 @@ if mode.__eq__("U"):
                 print("Current Score: {}".format(score))
                 print("Current Reward: {}".format(reward))
                 print("Done: {}".format(done))
-                imgplot = plt.imshow(state[0])
+                if modeltype != 1:
+                    imgplot = plt.imshow(state[0][0])
+                    print("State inputs other than board: {}, {}".format(state[1][0], state[2][0]))
+                else:
+                    imgplot = plt.imshow(state[0])
                 plt.pause(0.001)
             if done:
                 alive = False
@@ -285,7 +316,7 @@ defining the AI training model
 
 if mode.__eq__("T") or mode.__eq__("M"):
     print("Loading AI model class...")
-    from keras import layers, optimizers, models
+    from keras import layers, optimizers, models, Input
     from collections import deque
     
     class AIPlayer:
@@ -294,27 +325,50 @@ if mode.__eq__("T") or mode.__eq__("M"):
         def __init__(self, numStateInputs, numActions):
             self.numStateInputs = numStateInputs
             self.numActions = numActions
-            self.memory = deque(maxlen=2000)
+            self.memory = deque(maxlen=20000)
             self.gamma = 0.97    # discount rate for future events
             self.epsilon = 1.0  # exploration rate
             self.epsilon_min = 0.01
-            self.epsilon_decay = 0.997
+            self.epsilon_decay = 0.9988
             self.learning_rate = 0.001
-            self.model = self.getModel()
-    
-        def getModel(self):
+            if modeltype == 1:
+                self.model = self.getModel1()
+            else:
+                self.model = self.getModel2()
+            
+        def getModel1(self):
             # Neural Net for Deep-Q learning Model
             model = models.Sequential()
-            model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=self.numStateInputs[0]))
-            model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-            model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+            model.add(layers.Conv2D(16, (1, 1), activation='relu', input_shape=self.numStateInputs[0]))
+            model.add(layers.Conv2D(16, (3, 3), strides=2, activation='relu'))
+            model.add(layers.Conv2D(32, (3, 3), strides=2, activation='relu'))
             #now to flatten the list of 3D vectors into a list of 1D vectors for Dense layers
             model.add(layers.Flatten())
-            model.add(layers.Dense(32, activation='relu'))
+            model.add(layers.Dense(256, activation='relu'))
             model.add(layers.Dense(self.numActions, activation='linear'))
             model.compile(loss='mse',
                           optimizer=optimizers.Adam(lr=self.learning_rate))
             return model
+        
+        def getModel2(self):
+            # Neural Net for Deep-Q learning Model
+            boardInput = Input(shape=self.numStateInputs[0])
+            board1 = layers.Conv2D(16, (1, 1), activation='relu')(boardInput)
+            board2 = layers.Conv2D(16, (3, 3), strides=2, activation='relu')(board1)
+            board3 = layers.Conv2D(32, (3, 3), strides=2, activation='relu')(board2)
+            #now to flatten the list of 3D vectors into a list of 1D vectors for Dense layers
+            board4 = layers.Flatten()(board3)
+            board5 = layers.Dense(256, activation='relu')(board4)
+            radarInput = Input(shape=self.numStateInputs[1])
+            radar1 = layers.Dense(16, activation='relu')(radarInput)
+            dispInput = Input(shape=self.numStateInputs[2])
+            disp1 = layers.Dense(16, activation='relu')(dispInput)
+            concat = layers.concatenate([board5, radar1, disp1])
+            actions = layers.Dense(self.numActions, activation='linear')(concat)
+            fullmodel = models.Model([boardInput, radarInput, dispInput], actions)
+            fullmodel.compile(loss='mse',
+                          optimizer=optimizers.Adam(lr=self.learning_rate))
+            return fullmodel
     
         def remember(self, state, action, reward, next_state, done):
             self.memory.append((state, action, reward, next_state, done))
@@ -352,25 +406,52 @@ let a trained model play with the following functions
 if mode.__eq__("M"):
     print("Loading functions to load and play with a pre-trained AI model")
     def playSnake(player):
-        gameEnv = Snake(boardSize=20, startingSize=4)
-        state = gameEnv.reset() # get initial state
-        tick, score = gameEnv.getGameInfo()
+        savingBoardHistory = False
+        print("Save board history? (y/n)")
+        usrIn = input()
+        if usrIn.__eq__("y"):
+            savingBoardHistory = True
+            print("Save to where? (path without last slash, i.e. ~ or /home/usr)")
+            boardSavePath = input()
+            print("Saving board history to: {}".format(boardSavePath))
+        else:
+            print("Not saving board history")
+        
+        env = Snake(boardSize=20, startingSize=4)
+        state = env.reset() # get initial state
+        tick, score = env.getGameInfo()
         print("Current Tick: {}".format(tick))
         print("Current Score: {}".format(score))
-        reward = gameEnv.getCurrentReward()
+        reward = env.getCurrentReward()
         print("Current Reward: {}".format(reward))
+        print("___________________________________________")
+        if savingBoardHistory:
+            cboardSavePath = boardSavePath+"/Game{:06d}".format(game)
+            os.mkdir(cboardSavePath)
+            if modeltype == 1:
+                np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), state)
+            else:
+                np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), state[0])
         done = False
         while not done:
             action = player.act(state, exploring=False) # get the action the AI wants to do
             print("Taking action: {}".format(action))
-            nextState, reward, done = gameEnv.takeAction(action) # collect the results from taking the action
-            tick, score = gameEnv.getGameInfo()
+            nextState, reward, done = env.takeAction(action) # collect the results from taking the action
+            tick, score = env.getGameInfo()
             print("Current Tick: {}".format(tick))
             print("Current Score: {}".format(score))
             print("Current Reward: {}".format(reward))
             print("___________________________________________")
-            state = nextState
-        print("Final Score: {}".format(gameEnv.score))
+            if savingBoardHistory:
+                if modeltype == 1:
+                    np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), nextState)
+                else:
+                    np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), nextState[0])
+        state = nextState
+        score = env.score
+        if savingBoardHistory:
+            os.rename(cboardSavePath, cboardSavePath+"Score{:03d}".format(score))
+        print("Final Score: {}".format(env.score))
         
     print("Type full path to model save that you wish to load: ")
     path = input()
@@ -419,7 +500,7 @@ if mode.__eq__("T"):
     numActions = env.numActions
     player = AIPlayer(numStateInputs, numActions)
     done = False
-    batch_size = 64
+    batch_size = 32
     highestScore = 0
     
     for game in range(1, NumTrainGames+1):
@@ -429,12 +510,16 @@ if mode.__eq__("T"):
         tick, score = env.getGameInfo()
         print("Current Tick: {}".format(tick))
         print("Current Score: {}".format(score))
+        reward = env.getCurrentReward()
+        print("Current Reward: {}".format(reward))
         print("___________________________________________")
         if savingBoardHistory:
-            reward = env.getCurrentReward()
             cboardSavePath = boardSavePath+"/Game{:06d}".format(game)
             os.mkdir(cboardSavePath)
-            np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), state)
+            if modeltype == 1:
+                np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), state)
+            else:
+                np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), state[0])
         done = False
         while not done:
             action = player.act(state) # get the action the AI wants to do
@@ -449,7 +534,10 @@ if mode.__eq__("T"):
             print("Done: {}".format(done))
             print("___________________________________________")
             if savingBoardHistory:
-                np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), nextState)
+                if modeltype == 1:
+                    np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), nextState)
+                else:
+                    np.save(cboardSavePath+"/Tick{:05d}Score{:03d}Reward{:03d}".format(tick, score, reward), nextState[0])
             player.remember(state, action, reward, nextState, done)
             state = nextState
             if len(player.memory) > batch_size:
@@ -464,6 +552,10 @@ if mode.__eq__("T"):
             print('NEW HIGH SCORE: {}!'.format(score))
             if savingModel:
                 player.save(modelSavePath+"/Score{:03d}".format(highestScore))
+    # save the last model once training is over
+    if savingModel:
+        print("Saving final model now that training is over!")
+        player.save(modelSavePath+"/FinalModel")
             
 #%%
 '''
@@ -485,6 +577,6 @@ if mode.__eq__("W"):
             im = plt.imshow(np.load(gamePath+"\\"+gameFolder+"\\"+gameTick)[0], animated=True)
             text = title.text(0.5, 19, 'G: '+gameFolder[4:10]+' T: '+gameTick[4:9]+' S: '+gameTick[14:17]+' R: '+gameTick[23:-4], size='x-large', va='bottom', ha='left', color='w')
             images.append([im, text])
-    ani = animation.ArtistAnimation(fig, images, interval=25, blit=True, repeat=False)
+    ani = animation.ArtistAnimation(fig, images, interval=25, blit=True, repeat_delay=1000)
     #ani.save(path)
     plt.show()
